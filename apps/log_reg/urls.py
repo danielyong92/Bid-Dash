2f6ac973e29a3b9fd4179873a46fd8ef7eb8ce8a
Tuan Nguyen <ntuan2709@gmail.com>
2019-04-24 10:15:49 -0500

    Morning Wed

--- apps/log_reg/urls.py
+++ apps/log_reg/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
                     
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^login$', views.login),
     url(r'^signup$', views.signup),
     url(r'^register$', views.regacc),
-    # url(r'^login$', views.logacc),
+    url(r'^loginbt$', views.logacc),
     url(r'^dashboard$', views.dashboard),
     url(r'^logout$', views.logout),
 ]
