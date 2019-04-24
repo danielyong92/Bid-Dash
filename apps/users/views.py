8c2655ea23d3b9db074ecf71e4402bbb37a8d2c9
James Owens <jamesowens1000@hotmail.com>
2019-04-24 15:31:49 -0500

    JO - Wednesday afternoon commit

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,22 +1,34 @@
 from django.shortcuts import render, HttpResponse
 from mysqlconnection import connectToMySQL
+from django.contrib.auth import login, authenticate
 import bcrypt
-from .models import *
 
 def profile(request):
+    if request.session['logged'] == False:
+        return redirect("/")
+    elif request.session['logged'] == True:
+        mysql = connectToMySQL('bid_dash')
 
-    mysql = connectToMySQL('bid_dash')
+        data = {'user_id': request.session['user_id']}
+        # data = {'user_id': 1} #for testing the user_id
+        user = mysql.query_db('SELECT * FROM users WHERE id = %(user_id)s;', data)
 
-    data = {'user_id': 1}
-    user = mysql.query_db('SELECT * FROM users WHERE id = %(user_id)s;', data)
+        mysql = connectToMySQL('bid_dash')
+        jobs = mysql.query_db('SELECT * FROM jobs WHERE users_id = %(user_id)s ORDER BY end_datetime;', data)
 
-    # jobs = mysql.query_db('SELECT * FROM jobs WHERE users_id = %(user_id)s ORDER BY end_datetime;', data)
-    jobs = mysql.query_db('SELECT * FROM jobs;')
+        mysql = connectToMySQL('bid_dash')
+        bids = mysql.query_db('SELECT * FROM bids JOIN jobs ON bids.jobs_id = jobs.id WHERE bids.users_id = %(user_id)s ORDER BY jobs.end_datetime;', data)
 
-    context = {
-        "user": user[0],
-        # "jobs": []
-    }
-    print(user[0])
+        context = {
+            "user": user[0],
+            "jobs": jobs,
+            "job_count": len(jobs),
+            "bids": bids,
+            "bid_count": len(bids)
+        }
 
     return render(request, "users/profile.html", context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
