85c3d8f9027c4f9fd06cc1f137b73edd0b24d252
Tuan Nguyen <ntuan2709@gmail.com>
2019-04-24 18:25:33 -0500

    Update settings.py

--- biddash/settings.py
+++ biddash/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'biddash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bid_dash',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
