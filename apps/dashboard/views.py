11d3e05a13cf4cf1000fac24e8b44a6725fea00c
James Owens <jamesowens1000@hotmail.com>
2019-04-25 17:03:01 -0500

    JO - Thursday evening commit

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -14,14 +14,41 @@ def dashboard(request):
     else:
         mysql = connectToMySQL('bid_dash')
         data = {
-        'iduser': request.session['user_id']
+            'iduser': request.session['user_id']
         }
         user = mysql.query_db("SELECT * FROM users WHERE id=%(iduser)s;", data)
-        # print("First",user[0]["first_name"])
-        # print("Last",user[0]["last_name"])
-        # print(user)
+
         mysql = connectToMySQL('bid_dash')
         jobs = mysql.query_db('SELECT j.*, a.city, u.first_name, u.last_name FROM jobs j JOIN addresses a ON j.addresses_id = a.id JOIN users u ON j.users_id = u.id ORDER BY j.end_datetime;')
+        content = {
+            "user": user,
+            "first_name": user[0]["first_name"],
+            "last_name": user[0]["last_name"],
+            "jobs": jobs
+        }
+    return render(request, "dashboard/dashboard.html", content)
+
+def dashboard_cat(request,cat_id):
+    if 'user_id' not in request.session:
+        mysql = connectToMySQL('bid_dash')
+        jobs = mysql.query_db('SELECT j.*, a.city, u.first_name, u.last_name FROM jobs j JOIN addresses a ON j.addresses_id = a.id JOIN users u ON j.users_id = u.id ORDER BY j.end_datetime;')
+        content = {
+            "jobs": jobs
+        }
+    else:
+        mysql = connectToMySQL('bid_dash')
+        data = {
+            'iduser': request.session['user_id']
+        }
+        user = mysql.query_db("SELECT * FROM users WHERE id=%(iduser)s;", data)
+
+        mysql = connectToMySQL('bid_dash')
+        data = {
+            'cat_id': cat_id
+        }
+        print('CAT ID:', cat_id)
+        jobs = mysql.query_db('SELECT j.*, a.city, u.first_name, u.last_name FROM jobs j JOIN addresses a ON j.addresses_id = a.id JOIN users u ON j.users_id = u.id WHERE category_id = %(cat_id)s ORDER BY j.end_datetime;',data)
+        print('ALL JOBS:',jobs)
         content = {
             "user": user,
             "first_name": user[0]["first_name"],
